afdd3955a9d8a2bf07c72fcfde4b88753c74bac6
skim <rumex13@naver.com>
2022-11-20 15:46:50 +0900

    add semantic ui

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 
 ## Packages
 - react-router-dom
+- semantic-ui-react
 
 ## Available Scripts
 
--- template/src/components/App.tsx
+++ template/src/components/App.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
-import About from './About';
-import Home from './Home';
-import Item from './Item';
+import Login from './Login';
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/item/:id' element={<Item/>}/>
-      <Route path='/about' element={<About/>}/>
+      <Route path='/' element={<Login/>}/>
       <Route path="*" element={<Navigate to ="/" />}/>
     </Routes>
   );
